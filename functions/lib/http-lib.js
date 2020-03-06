e57bdaf949ea352919dade077e1737be1ebf9fd2
Martin <chetinchog@yahoo.com>
2020-03-06 00:47:49 -0300

    CORS...

--- functions/lib/http-lib.js
+++ functions/lib/http-lib.js
@@ -1,10 +1,15 @@
 const response = (res, message, code = undefined) => {
-  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Allow", "*");
+  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
   res.set(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
-  res.set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
+  res.set(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, X-Auth-Token, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+  );
+  res.set("Access-Control-Allow-Credentials", "true");
   res.status(code || 200).send(message);
 };
 
